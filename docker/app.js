6e8a59dadf145450cc8316735e4a7a798baa2424
adenicole <dedoyinnicole@gmail.com>
2021-07-21 10:39:11 +0100

    Updated Dockerfile and app.js

--- docker/app.js
+++ docker/app.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome to SCA Cloud School Application');
-}).listen(1234);
+}).listen(3030);
 
